2845ea5fde18af94d309dfa57841d16d1046ad24
Andrew Fergusson <andrew.fergusson@ons.gov.uk>
2022-02-22 15:20:19 +0000

    Test pipeline

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Simple bash script
+
+echo $PWD
+
+for d in `ls` ; do
+    if [ -d $d ] ; then
+        echo "Found csvcubed target in ./$d/"
+        if [ -d $d/out ] ; then
+            echo "Found out folder, skipping. Delete the folder if you want to rebuild."
+        else
+            cd $d
+            infojson2csvqb build observations.csv -c info.json -m catalog-metadata.json
+            git add out/* && git commit -m "Built csv-w for $d"
+            cd ..
+        fi
+    else
+        echo "Outputs found in $d skipping."
+    fi
+done
+
